e9560a942de1d0864febae1dca6f7f83dea6b818
fengq_msi <1324396762@qq.com>
2023-04-29 20:21:46 +0800

    flag.Parse()

--- main.go
+++ main.go
@@ -18,7 +18,9 @@ func main() {
 	receiveMailbox := flag.String("receiveMailbox", "", "receive mailbox")
 	password := flag.String("password", "", "your mailbox Authorization code")
 	cc := flag.String("cc", "", "Cc person")
-	token := flag.String("token", "a", "token")
+	token := flag.String("token", "", "token")
+	flag.Parse()
+
 	repository := os.Getenv("GITHUB_REPOSITORY")
 	repoParts := strings.Split(repository, "/")
 	if *token == "" {
@@ -27,8 +29,9 @@ func main() {
 	}
 	user := repoParts[0]
 	repo := repoParts[1]
-
 	graphqlResponse := getGithubProjectInfo(*token, user, repo)
+
+	fmt.Println(graphqlResponse)
 	lastUser := graphqlResponse.Data.Repository.Stargazers.Edges[0].Node
 	subject := fmt.Sprintf("%s started", repository)
 	content := fmt.Sprintf(`<div style="text-align: center;">   
